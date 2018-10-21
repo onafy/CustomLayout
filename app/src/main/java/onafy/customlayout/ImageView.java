891bc04e73d45ebe93626f5ceec69050c69d6a49
azizahnovitami <ccghy@gmail.com>
2018-10-21 12:34:09 +0800

    Initial commit

---
+++ app/src/main/java/onafy/customlayout/ImageView.java
@@ -0,0 +1,13 @@
+package onafy.customlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_view);
+    }
+}
